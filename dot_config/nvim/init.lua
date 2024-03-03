b9a08a72df0e0a3688121841004f1ec429700643
nakxkri@shinishiho.cyou <nakxkri@shinishiho.cyou>
2024-03-03 21:34:42 +0700

    Add a bunch nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,5 @@
-require("shinishiho.remap")
-require("shinishiho.set")
+require("remap")
+require("set")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -14,65 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                ensure_installed = { "c", "lua", "javascript", "typescript",
-                    "rust", "python", "html", "latex"},
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    },
-    {'theprimeagen/harpoon'},
-    {'mbbill/undotree'},
-    {'tpope/vim-fugitive'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {
-        'hrsh7th/nvim-cmp',
-        config = function ()
-            require'cmp'.setup {
-                snippet = {
-                    expand = function(args)
-                        require'luasnip'.lsp_expand(args.body)
-                    end
-                },
-
-                sources = {
-                    { name = 'luasnip' },
-                    { name = 'copilot' },
-                    -- more sources
-                },
-            }
-        end
-    },
-    {'L3MON4D3/LuaSnip'},
-    {'lervag/vimtex'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'github/copilot.vim'},
-    {
-        "folke/zen-mode.nvim",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    }
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup({
+    spec = "plugins",
+    change_detection = { enabled = false } })
