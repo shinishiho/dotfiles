779557fd89c629de6cc33348d20e6c81b47f0f80
ShiniShiho <nakxkri@shinishiho.cyou>
2024-06-05 10:49:38 +0700

    backup 05-06-2024

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -6,14 +6,14 @@ return {
         local interval = 15000
         local theme_tabl =
         {
-            ["Catppuccin Mocha"] = "catppuccin-mocha",
-            ["Catppuccin Latte"] = "catppuccin-latte",
-            ["Decay Green"] = "",
-            ["Rosé Pine"] = "rose-pine",
-            ["Material Sakura"] = "rose-pine-dawn",
-            ["Frosted Glass"] = "catppuccin-latte",
-            ["Gruvbox Retro"] = "gruvbox-dark-medium",
-            ["Tokyo Night"] = "tokyo-night-storm",
+            ["Catppuccin-Mocha"] = "catppuccin-mocha",
+            ["Catppuccin-Latte"] = "catppuccin-latte",
+            ["Decay-Green"] = "",
+            ["Rose-Pine"] = "rose-pine",
+            ["Material-Sakura"] = "rose-pine-dawn",
+            ["Frosted-Glass"] = "catppuccin-latte",
+            ["Gruvbox-Retro"] = "gruvbox-dark-medium",
+            ["Tokyo-Night"] = "tokyonight-storm",
         }
 
         local current = vim.g.colors_name
@@ -24,10 +24,9 @@ return {
         end
 
         local function get_system_theme()
-            local cmd = "source /home/nakxkri/.local/share/bin/globalcontrol.sh && echo $hydeTheme"
+            local cmd = "grep -oP '1\\|[^\\|]*\\|' $HOME/.config/hypr/theme.ctl | cut -d'|' -f2"
             utils.start_job(cmd, {
                 on_stdout = function(data)
-                    print(data[1])
                     check_and_change(data[1])
                 end,
             })
